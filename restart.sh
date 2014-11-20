5c1f5617a2d77171e6f15651115af9f3bf99b4d7
Julen Landa Alustiza <julen@zokormazo.info>
2014-11-20 18:44:09 +0100

    Fix logic & replace hardcoded settings with global variables

--- restart.sh
+++ restart.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-#ping google.com and restart cisco EPC3825 router if no response
+# ping google.com and restart cisco EPC3825 router if no response
+
+# config
+ROUTER_URL="http://192.168.13.1"	# root URL to router web page
+ROUTER_USERNAME="admin"			# router username
+ROUTER_PASSWORD="cisco"			# router password
+COOKIE_FILE="/tmp/cookie.test"
 
 ping -q -c1 google.com > /dev/null
 
 if [ $? -ne 0 ] ; then
-	rm /tmp/cookie
-	curl -L -c /tmp/cookie -d "username_login=admin" -d "password_login=cisco" -d "login=Log+In" http://192.168.13.1/goform/Docsis_system
-	curl --cookie /tmp/cookie --referer http://192.158.13.1/Devicerestart.asp -d "mtenRestore=Device+Restart" -d "devicerestart=1" http://192.168.13.1/goform/Devicerestart
+	if [ ! -f ${COOKIE_FILE} ] ; then
+		curl -L -c ${COOKIE_FILE} -d "username_login=${ROUTER_USERNAME}" -d "password_login=${ROUTER_PASSWORD}" -d "login=Log+In" ${ROUTER_URL}/goform/Docsis_system > /dev/null
+		curl -L -c ${COOKIE_FILE} /tmp/cookie --referer ${ROUTER_URL}/Devicerestart.asp -d "mtenRestore=Device+Restart" -d "devicerestart=1" ${ROUTER_URL}/goform/Devicerestart > /dev/null
+		rm -f ${COOKIE_FILE}
+	fi
 fi
